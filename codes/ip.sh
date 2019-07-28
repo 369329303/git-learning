908b67fe231e90c868a224541f98ee21ff5bc4ee
xueyuncheng <datouguix@gmail.com>
2019-07-28 09:38:18 +0800

    update.

---
+++ codes/ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must run as root!"
+    exit 1
+fi
+
+ip route del default via 192.168.1.1 dev enp2s0
+ip addr add 10.0.80.106/24 dev enp2s0
+ip route add 10.0.0.0/16 via 10.0.80.254 dev enp2s0
