ca6c7f16b9f3e8687c7a004c378f7759c42991d0
Jack <datouguix@gmail.com>
2020-05-15 11:35:24 +0800

    update

--- codes/vpn.sh
+++
@@ -1,3 +0,0 @@
-sudo ip route del default via 192.168.43.1 dev wlp3s0 proto dhcp metric 600
-sudo ip route add default via 192.168.43.1 dev wlp3s0 proto dhcp metric 60
-sudo ip route add 10.0.0.0/16 via 10.0.80.254 dev enp2s0
