6e2b016c4dd7e595259bd97fb11feee884f00d12
Jack <datouguix@gmail.com>
2020-06-16 11:33:04 +0800

    添加raft协议

--- codes/ipfoward.sh
+++ codes/ipfoward.sh
@@ -4,11 +4,11 @@ iptables -t nat -F
 iptables -t filter -F
 
 
-iptables -t nat -A PREROUTING -p tcp --dport 8001 -j DNAT --to-destination 64.227.42.205:8001
-iptables -t nat -A PREROUTING -p udp --dport 8001 -j DNAT --to-destination 64.227.42.205:8001
+iptables -t nat -A PREROUTING -p tcp --dport 8009 -j DNAT --to-destination 64.227.42.205:8001
+iptables -t nat -A PREROUTING -p udp --dport 8001 -j DNAT --to-destination 64.227.42.205:8010
 iptables -t nat -A PREROUTING -p tcp --dport 8002 -j DNAT --to-destination 64.227.42.205:443
 iptables -t nat -A PREROUTING -p udp --dport 8002 -j DNAT --to-destination 64.227.42.205:443
-iptables -t nat -A POSTROUTING -p all -d 64.227.42.205 -j SNAT --to-source 172.17.0.214
+iptables -t nat -I POSTROUTING -p all -d 64.227.42.205 -j SNAT --to-source 172.17.0.214
 
 iptables -I INPUT -s 140.205.201.0/28 -j DROP
 iptables -I INPUT -s 140.205.201.16/29 -j DROP
